c1f1407304d6610e7e7e89d5d079a8118d19e6cb
Frank Li <lznuaa@gmail.com>
2009-09-10 21:42:33 +0800

    Prompt dialog to ask passwords
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- compat/msvc.c
+++ compat/msvc.c
@@ -2,6 +2,7 @@
 #include "win32.h"
 #include <conio.h>
 #include "../strbuf.h"
+#include "resource.h"
 
 DIR *opendir(const char *name)
 {
@@ -30,6 +31,64 @@ int closedir(DIR *dir)
 	_findclose(dir->dd_handle);
 	free(dir);
 	return 0;
+}
+
+
+#define PASSWD_MAX 128
+static char *g_prompt;
+static char g_passwd[PASSWD_MAX];
+
+BOOL CALLBACK passwd_dlg_proc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg == WM_INITDIALOG)
+	{
+		// Set prompt text
+		HWND prompt;
+		prompt=GetDlgItem(hwndDlg,IDC_PROMPT);
+		SetWindowText(prompt,g_prompt);
+
+		// Make sure edit control has the focus
+		if (GetDlgCtrlID((HWND) wParam) != IDC_PASSWORD)
+		{ 
+			SetFocus(GetDlgItem(hwndDlg, IDC_PASSWORD));
+			return FALSE; 
+		} 
+		return TRUE; 
+	}
+	else if (uMsg == WM_COMMAND && LOWORD(wParam) == IDCANCEL && HIWORD(wParam) == BN_CLICKED)
+	{
+		EndDialog(hwndDlg, IDCANCEL);
+		return 1;
+	}
+	else if (uMsg == WM_COMMAND && LOWORD(wParam) == IDOK && HIWORD(wParam) == BN_CLICKED)
+	{
+
+		HWND passwd;
+		passwd=GetDlgItem(hwndDlg,IDC_PASSWORD);
+		GetWindowText(passwd,g_passwd,PASSWD_MAX);
+		EndDialog(hwndDlg, IDOK);
+		return 1;
+	}
+	return 0;
+}
+
+char *vc_getpass(const char *prompt)
+{
+	DWORD error=0;
+	HINSTANCE hmodule=0;
+
+	hmodule = GetModuleHandle(0);
+	g_prompt = prompt;
+	memset(g_passwd,0, PASSWD_MAX);
+	if( DialogBoxParam(hmodule, MAKEINTRESOURCE(IDD_PASSWORD), 0,
+                  (DLGPROC)(passwd_dlg_proc), 0) == IDOK)
+		return g_passwd;
+
+	error=GetLastError();
+
+	g_passwd[0] = 0;
+	return g_passwd;
+
 }
 
 #include "mingw.c"
