837581e302e7e6b1ef6e8d528b449a68eba6039a
Sven Strickroth <email@cs-ware.de>
2013-05-09 01:10:41 +0200

    Use git system config based on the configured git path in TortoiseGit
    Related to issue #1184.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- attr.c
+++ attr.c
@@ -868,7 +868,7 @@ static const char *git_etc_gitattributes(void)
 {
 	static const char *system_wide;
 	if (!system_wide)
-		system_wide = system_path(ETC_GITATTRIBUTES);
+		system_wide = system_path("..\\" ETC_GITATTRIBUTES);
 	return system_wide;
 }
 
