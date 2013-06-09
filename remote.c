e28892da25f04d38f2d5ade7d14ef9b84a3c01ea
Sven Strickroth <email@cs-ware.de>
2013-06-09 06:38:37 +0200

    Make libgit generally compile
    Signed-off-by: Sven Strickroth <email@cs-ware.de>
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- remote.c
+++ remote.c
@@ -14,6 +14,7 @@
 #include "argv-array.h"
 #include "commit-reach.h"
 #include "advice.h"
+#include <sal.h>
 
 enum map_direction { FROM_SRC, FROM_DST };
 
@@ -1606,8 +1607,7 @@ int branch_merge_matches(struct branch *branch,
 	return refname_match(branch->merge[i]->src, refname);
 }
 
-__attribute__((format (printf,2,3)))
-static const char *error_buf(struct strbuf *err, const char *fmt, ...)
+static const char *error_buf(struct strbuf *err, _Printf_format_string_ const char *fmt, ...)
 {
 	if (err) {
 		va_list ap;
