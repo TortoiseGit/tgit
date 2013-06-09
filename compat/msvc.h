25da88f3a8fb03ec7911a3db14d7fb39369b54f0
Sven Strickroth <email@cs-ware.de>
2013-06-09 06:38:37 +0200

    Make libgit generally compile
    Signed-off-by: Sven Strickroth <email@cs-ware.de>
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- compat/msvc.h
+++ compat/msvc.h
@@ -28,6 +28,27 @@ typedef int sigset_t;
 /* open for reading, writing, or both (not in fcntl.h) */
 #define O_ACCMODE     (_O_RDONLY | _O_WRONLY | _O_RDWR)
 
-#include "compat/mingw.h"
+#include "mingw.h"
 
+/* Git runtime infomation */
+#define RUNTIME_PREFIX
+#define FALLBACK_RUNTIME_PREFIX ""
+#define PREFIX "."
+#define BINDIR "bin"
+
+#define ETC_GITCONFIG "etc\\gitconfig"
+#define ETC_GITATTRIBUTES "etc\\gitattributes"
+#define GIT_EXEC_PATH "bin"
+#define GIT_MAN_PATH "man"
+#define GIT_INFO_PATH "info"
+#define GIT_HTML_PATH "doc\\git\\html"
+#define DEFAULT_GIT_TEMPLATE_DIR "share\\git-core\\templates"
+#endif
+
+/* Git version infomation */
+#ifndef __MSVC__VERSION
+#define __MSVC__VERSION
+#define GIT_VERSION "2.21.0"
+#define GIT_USER_AGENT "git/" GIT_VERSION
+#define GIT_BUILT_FROM_COMMIT "(unknown)"
 #endif
