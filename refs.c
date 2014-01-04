4a5169498476423111106ea21d9909d97b656718
Sven Strickroth <email@cs-ware.de>
2014-01-04 16:08:56 +0100

    Re-implement invalidate_ref_cache and reset cached paths
    This basically reverts commit 746593bdcaf86827e016c97e091442d6a227de0f and extends it for libgit >= 2.13.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- refs.c
+++ refs.c
@@ -1549,6 +1549,11 @@ static struct ref_store_hash_entry *alloc_ref_store_hash_entry(
 /* A pointer to the ref_store for the main repository: */
 static struct ref_store *main_ref_store;
 
+void gimme_main_ref_store(struct ref_store** refs)
+{
+	*refs = main_ref_store;
+}
+
 /* A hashmap of ref_stores, stored by submodule name: */
 static struct hashmap submodule_ref_stores;
 
