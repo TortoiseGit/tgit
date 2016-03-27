c9d2c9d2ba44960754fd5b04f123939d623db258
Sven Strickroth <email@cs-ware.de>
2016-03-28 00:28:22 +0200

    Use a separate environment in libgit
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- compat/mingw.h
+++ compat/mingw.h
@@ -476,6 +476,7 @@ extern int (*win32_is_mount_point)(struct strbuf *path);
 #define PATH_SEP ';'
 char *mingw_query_user_email(void);
 #define query_user_email mingw_query_user_email
+extern void build_libgit_environment(void);
 extern const char *program_data_config(void);
 #define git_program_data_config program_data_config
 #if !defined(__MINGW64_VERSION_MAJOR) && (!defined(_MSC_VER) || _MSC_VER < 1800)
