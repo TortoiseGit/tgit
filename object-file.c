b66b5703c449a58bb087c2d087500841bbfcdad8
Sven Strickroth <email@cs-ware.de>
2022-02-14 12:48:14 +0100

    Skip conversion filters
    As a preparation to Git update to 2.34.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- object-file.c
+++ object-file.c
@@ -1195,8 +1195,8 @@ static int index_stream_convert_blob(struct index_state *istate,
 	assert(path);
 	ASSERT(would_convert_to_git_filter_fd(istate, path));
 
-	convert_to_git_filter_fd(istate, path, fd, &sbuf,
-				 get_conv_flags(flags));
+	/* convert_to_git_filter_fd(istate, path, fd, &sbuf,
+				 get_conv_flags(flags));*/
 
 	if (write_object)
 		ret = write_object_file(sbuf.buf, sbuf.len, OBJ_BLOB,
