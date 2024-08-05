b44c7082b1e03da94a0541666a9289a047f4ed1b
Sven Strickroth <email@cs-ware.de>
2024-08-05 13:54:31 +0200

    No need to set to NULL
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- refs/files-backend.c
+++ refs/files-backend.c
@@ -154,7 +154,7 @@ static struct files_ref_store *files_downcast(struct ref_store *ref_store,
 static void files_ref_store_release(struct ref_store *ref_store)
 {
 	struct files_ref_store *refs = files_downcast(ref_store, 0, "release");
-	clear_loose_ref_cache(refs);
+	free_ref_cache(refs->loose);
 	free(refs->gitcommondir);
 	ref_store_release(refs->packed_ref_store);
 	free(refs->packed_ref_store);
