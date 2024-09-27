szeder/all-git-forks

Track all open file handles
Signed-off-by: Sven Strickroth <email@cs-ware.de>
Signed-off-by: Frank Li <lznuaa@gmail.com>

compat/mingw.c
@@ -1,3 +1,4 @@
+#include <cache.h>
 #include "../git-compat-util.h"
 #include "win32.h"
 #include <conio.h>
@@ -5,6 +6,89 @@
 #include "../run-command.h"
 
 static const int delay[] = { 0, 1, 10, 20, 40 };
+
+#include <shellapi.h>
+
+#define OPEN_HANDLE	1
+#define OPEN_FILE	2
+
+struct open_data
+{
+	long long	data;
+	int			flag;
+};
+
+static struct open_data *p_opened_file_handle;
+static int p_opened_file_size;
+static int p_alloc_size;
+
+void add_handle(long long x, int flag)
+{
+	int i = 0;
+	struct open_data handle;
+	handle.data = x;
+	handle.flag = flag;
+
+	if (p_opened_file_handle == NULL)
+		ALLOC_GROW(p_opened_file_handle, p_opened_file_size + 10, p_alloc_size);
+
+	for (i = 0; i < p_opened_file_size; ++i)
+	{
+		if (p_opened_file_handle[i].data == handle.data && p_opened_file_handle[i].flag == handle.flag)
+			return;
+	}
+
+	ALLOC_GROW(p_opened_file_handle, p_opened_file_size + 1, p_alloc_size);
+	p_opened_file_handle[p_opened_file_size++] = handle;
+}
+
+int remove_handle(long long x, int flag)
+{
+	int i = 0;
+	struct open_data handle;
+	handle.data = x;
+	handle.flag = flag;
+
+	if (p_opened_file_handle == NULL)
+		return FALSE;
+
+	for (i = 0; i < p_opened_file_size; ++i)
+	{
+		if (p_opened_file_handle[i].data == handle.data && p_opened_file_handle[i].flag == handle.flag)
+		{
+			if (i != p_opened_file_size - 1)
+				memmove(p_opened_file_handle + i, p_opened_file_handle + i + 1, (p_opened_file_size - i - 1) * sizeof *(p_opened_file_handle));
+
+			--p_opened_file_size;
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+#undef close
+#undef fclose
+
+void close_all()
+{
+	int i = 0;
+	if (p_opened_file_handle== NULL)
+		return;
+
+	for (i = 0; i < p_opened_file_size; ++i)
+	{
+		if (p_opened_file_handle[i].flag == OPEN_HANDLE)
+			close(p_opened_file_handle[i].data);
+		
+		if (p_opened_file_handle[i].flag == OPEN_FILE)
+			fclose((FILE*)p_opened_file_handle[i].data);
+	}
+
+	free(p_opened_file_handle);
+	p_alloc_size = 0;
+	p_opened_file_size = 0;
+	p_opened_file_handle = 0;
+}
 
 int err_win_to_posix(DWORD winerr)
 {
@@ -301,6 +385,10 @@ int mingw_open (const char *filename, int oflags, ...)
 		if (attrs != INVALID_FILE_ATTRIBUTES && (attrs & FILE_ATTRIBUTE_DIRECTORY))
 			errno = EISDIR;
 	}
+
+	if (fd >= 0)
+		add_handle(fd, OPEN_HANDLE);
+
 	return fd;
 }
 
@@ -349,9 +437,15 @@ int mingw_fgetc(FILE *stream)
 #undef fopen
 FILE *mingw_fopen (const char *filename, const char *otype)
 {
+	FILE *file;
 	if (filename && !strcmp(filename, "/dev/null"))
 		filename = "nul";
-	return fopen(filename, otype);
+	file = fopen(filename, otype);
+
+	if (file)
+		add_handle((long long)file, OPEN_FILE);
+
+	return file;
 }
 
 #undef freopen
@@ -360,6 +454,13 @@ FILE *mingw_freopen (const char *filename, const char *otype, FILE *stream)
 	if (filename && !strcmp(filename, "/dev/null"))
 		filename = "nul";
 	return freopen(filename, otype, stream);
+}
+
+#undef fclose
+int mingw_fclose(FILE * stream)
+{
+	remove_handle((long long)stream, OPEN_FILE);
+	return fclose(stream);
 }
 
 #undef fflush
@@ -393,6 +494,19 @@ static inline long long filetime_to_hnsec(const FILETIME *ft)
 	long long winTime = ((long long)ft->dwHighDateTime << 32) + ft->dwLowDateTime;
 	/* Windows to Unix Epoch conversion */
 	return winTime - 116444736000000000LL;
+}
+#undef close
+int mingw_close(int fileHandle)
+{
+	if (fileHandle >= 0)
+	{
+		if (remove_handle(fileHandle, OPEN_HANDLE) == TRUE)
+			return close(fileHandle);
+		else
+			return FALSE;
+	}
+	else
+		return close(fileHandle);
 }
 
 static inline time_t filetime_to_time_t(const FILETIME *ft)

TortoiseGit/tgit

compat/mingw.c
@@ -30,6 +30,68 @@
 
 #define HCAST(type, handle) ((type)(intptr_t)handle)
 
+struct open_data
+{
+	int fd;
+};
+
+static struct open_data *p_opened_file_handle;
+static int p_opened_file_size;
+static int p_alloc_size;
+
+void add_handle(int fd)
+{
+	struct open_data handle;
+	handle.fd = fd;
+
+	if (!p_opened_file_handle)
+		ALLOC_GROW(p_opened_file_handle, p_opened_file_size + 10, p_alloc_size);
+
+	for (int i = 0; i < p_opened_file_size; ++i)
+	{
+		if (p_opened_file_handle[i].fd == handle.fd)
+			return;
+	}
+
+	ALLOC_GROW(p_opened_file_handle, p_opened_file_size + 1, p_alloc_size);
+	p_opened_file_handle[p_opened_file_size++] = handle;
+}
+
+int remove_handle(int fd)
+{
+	if (!p_opened_file_handle)
+		return FALSE;
+
+	for (int i = 0; i < p_opened_file_size; ++i)
+	{
+		if (p_opened_file_handle[i].fd == fd)
+		{
+			if (i != p_opened_file_size - 1)
+				memmove(p_opened_file_handle + i, p_opened_file_handle + i + 1, (p_opened_file_size - i - 1) * sizeof *(p_opened_file_handle));
+
+			--p_opened_file_size;
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+#undef close
+
+void close_all(void)
+{
+	int i = 0;
+	if (!p_opened_file_handle)
+		return;
+
+	for (i = 0; i < p_opened_file_size; ++i)
+		close(p_opened_file_handle[i].fd);
+
+	FREE_AND_NULL(p_opened_file_handle);
+	p_alloc_size = 0;
+	p_opened_file_size = 0;
+}
+
 void open_in_gdb(void)
 {
 	static struct child_process cp = CHILD_PROCESS_INIT;
@@ -838,6 +900,10 @@ int mingw_open (const char *filename, int oflags, ...)
 		if (fd >= 0 && set_hidden_flag(wfilename, 1))
 			warning("could not mark '%s' as hidden.", filename);
 	}
+
+	if (fd >= 0)
+		add_handle(fd);
+
 	return fd;
 }
 
@@ -893,7 +959,23 @@ FILE *mingw_fopen (const char *filename, const char *otype)
 		errno = ENOENT;
 	if (file && hide && set_hidden_flag(wfilename, 1))
 		warning("could not mark '%s' as hidden.", filename);
+	if (file)
+		add_handle(fileno(file));
 	return file;
+}
+
+#undef close
+int mingw_close(int fileHandle)
+{
+	if (fileHandle >= 0)
+	{
+		if (remove_handle(fileHandle) == TRUE)
+			return close(fileHandle);
+
+		return FALSE;
+	}
+
+	return close(fileHandle);
 }
 
 FILE *mingw_freopen (const char *filename, const char *otype, FILE *stream)
@@ -921,6 +1003,15 @@ FILE *mingw_freopen (const char *filename, const char *otype, FILE *stream)
 	if (file && hide && set_hidden_flag(wfilename, 1))
 		warning("could not mark '%s' as hidden.", filename);
 	return file;
+}
+
+#undef fclose
+int mingw_fclose(FILE * stream)
+{
+	if (remove_handle(fileno(stream)) == TRUE)
+		return fclose(stream);
+
+	return FALSE;
 }
 
 #undef fflush